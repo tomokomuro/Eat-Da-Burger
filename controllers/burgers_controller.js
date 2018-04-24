e9ab6e19a9836b2874fe8e637bd8aceb7004b9b2
Tomoyuki Komuro <Tomo Komuro@tomos-macbook-pro-2.local>
2018-04-24 00:54:07 -0700

    what

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,32 +2,38 @@ var express = require("express");
 var router = express.Router();
 var burgers = require("../models/burger.js");
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     res.send('hi');
 })
 
-router.get('/index', function(req, res){
-	burgers.all(function(data){
-		var hbsObject = {burgers: data};
-		console.log(hbsObject);
-		res.render('index', hbsObject);
-	});
+router.get('/index', function (req, res) {
+    burgers.all(function (data) {
+        var hbsObject = { burgers: data };
+        console.log(hbsObject);
+        res.render('', hbsObject);
+    });
 });
 
-router.post("/", function(req, res) {
-    burger.create([
-   "name", "devoured"
- ], [
-   req.body.burger_name, req.body.devoured
- ], function() {
-   res.redirect("/");
- });
-})
-;
-router.post('/create', function(req, res){
-	burgers.create(['burger_name'], [req.body.b_name], function(data){
-		res.redirect('/burgers')
-	});
+// router.post("/burgers_update", function (req, res) {
+//     burger.create([
+//         "name", "devoured"
+//     ], [
+//             req.body.burger_name, req.body.devoured
+//         ], function () {
+//             res.redirect("/index");
+//         });
+// });
+
+router.post('/delete', function (req, res) {
+    burgers.delete(['burger_name'], [req.body.b_name], function (data) {
+        res.redirect('/index')
+    });
+});
+
+router.post('/create', function (req, res) {
+    burgers.create(['burger_name'], [req.body.b_name], function (data) {
+        res.redirect('/index')
+    });
 });
 
 module.exports = router;
